7245a769bea4fcca2804443921712012f786d05f
Matteo Rocco Micò <118108678+mrmYTAD@users.noreply.github.com>
2023-07-01 17:49:26 +0200

    Update basicvariables.h
    added idea for error handling, project paused, I will try to make a version with GUI

--- MOL-stable/code/basicvariables.h
+++ MOL-stable/code/basicvariables.h
@@ -7,3 +7,20 @@
 	- work with variables, change values.
 
 */
+
+// reading file:
+// ! if ch = /n n_newline++, n_gchar = 0.
+// every time i read char, n_gchar++
+
+// get ch
+
+// if ch == ''
+
+// else if
+
+// else if
+
+// else check variable.
+
+// if variable does not exist, then it's an error
+// error at line (n_newline)(n_gchar)
